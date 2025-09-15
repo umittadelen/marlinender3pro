1d3195d60afba393418f598a9d2e48c11eabeee6
umittadelen <umittadelen1277@gmail.com>
2025-09-15 06:19:41 +0200

    .

---
+++ buildroot/share/PlatformIO/scripts/STM32F103VE_longer.py
@@ -0,0 +1,29 @@
+import os, marlin
+Import("env")
+
+# Relocate firmware from 0x08000000 to 0x08010000
+marlin.relocate_firmware("0x08010000")
+
+custom_ld_script = os.path.abspath("buildroot/share/PlatformIO/ldscripts/STM32F103VE_longer.ld")
+for i, flag in enumerate(env["LINKFLAGS"]):
+    if "-Wl,-T" in flag:
+        env["LINKFLAGS"][i] = "-Wl,-T" + custom_ld_script
+    elif flag == "-T":
+        env["LINKFLAGS"][i + 1] = custom_ld_script
+
+# Rename ${PROGNAME}.bin and save it as 'project.bin' (No encryption on the Longer3D)
+def encrypt(source, target, env):
+    firmware = open(target[0].path, "rb")
+    marlin_alfa = open(target[0].dir.path +'/project.bin', "wb")
+    length = os.path.getsize(target[0].path)
+    position = 0
+    try:
+        while position < length:
+            byte = firmware.read(1)
+            marlin_alfa.write(byte)
+            position += 1
+    finally:
+        firmware.close()
+        marlin_alfa.close()
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", encrypt);
---
+++ buildroot/share/PlatformIO/scripts/creality.py
@@ -0,0 +1,13 @@
+import os, marlin
+Import("env")
+
+# Relocate firmware from 0x08000000 to 0x08007000
+marlin.relocate_firmware("0x08007000")
+
+custom_ld_script = os.path.abspath("buildroot/share/PlatformIO/ldscripts/creality.ld")
+
+for i, flag in enumerate(env['LINKFLAGS']):
+    if "-Wl,-T" in flag:
+        env['LINKFLAGS'][i] = "-Wl,-T" + custom_ld_script
+    elif flag == "-T":
+        env['LINKFLAGS'][i + 1] = custom_ld_script
